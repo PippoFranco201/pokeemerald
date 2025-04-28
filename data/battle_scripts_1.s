9c3b5f5d5fc8ce64a8a5e35234258a11ebc90404
Gianni <dragicfan@gmail.com>
2025-04-28 21:17:51 +0200

    modified:   src/data/text/abilities.h

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -4124,6 +4124,14 @@ BattleScript_DroughtActivates::
 	call BattleScript_WeatherFormChanges
 	end3
 
+BattleScript_SnowWarningActivates::
+	pause 0x20
+	printstring STRINGID_PKMNSXWHIPPEDUPHAILSTORM
+	waitstate
+	playanimation BS_BATTLER_0, B_ANIM_HAIL_CONTINUES, NULL
+	call BattleScript_WeatherFormChanges
+	end3
+
 BattleScript_TookAttack::
 	attackstring
 	pause B_WAIT_TIME_SHORT
